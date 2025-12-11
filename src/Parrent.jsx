2008a3bb0576171ebc47bb9000a186031b6089c0
sharon884 <sharonsharon9048461583@gmail.com>
2025-12-11 12:02:14 +0530

    created a  controlled component

--- src/Parrent.jsx
+++ src/Parrent.jsx
@@ -2,19 +2,34 @@ import React from "react";
 import { useState } from "react";
 import { Child } from "./Child";
 
-export function Parrent () {
+export function Parrent() {
 
 
-    const propfromChild = ( prop )=> {
-                  
-        setAge(prop)  
+    const propfromChild = (prop) => {
+
+        setAge(prop)
     };
-const [ age, setAge ] = useState(0);
-const [ name, SetName  ] = useState("sharon");
+
+
+    const [age, setAge] = useState(0);
+
+    const [name, SetName] = useState("sharon");
+    const [ dis, setDis ] = useState("");
+
+    const submit = () => {
+        setDis("");
+    }
+
+
     return (
         <>
-        <Child prop={name}  fuc={propfromChild}/>
-        <h2>{age}</h2>
+            <Child prop={name} fuc={propfromChild} />
+            <h2>{age}</h2>
+            
+            <input type="text" placeholder="type something" value={dis} onChange={(e)=> setDis(e.target.value)}  />
+            <h3>{dis}</h3>
+
+            <button onClick={()=> submit()}>submit</button>
         </>
     )
 };
