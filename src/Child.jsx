b04c893232975b96b94f663cae3cf712b4310e44
sharon884 <sharonsharon9048461583@gmail.com>
2025-12-11 11:49:34 +0530

    example for passing props from parrent to child

--- src/Child.jsx
+++ src/Child.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 
-export function Child ({prop}) {
+export function Child ({prop, fuc}) {
 
 
     let name = prop;
+  
+    const age = 20;
 
     return (
         <>
         <h1>{name}</h1>
+        <button onClick={() => fuc(age)}></button>
         </>
     )
 }
--- src/Parrent.jsx
+++ src/Parrent.jsx
@@ -4,10 +4,17 @@ import { Child } from "./Child";
 
 export function Parrent () {
 
+
+    const propfromChild = ( prop )=> {
+                  
+        setAge(prop)  
+    };
+const [ age, setAge ] = useState(0);
 const [ name, SetName  ] = useState("sharon");
     return (
         <>
-        <Child prop={name}/>
+        <Child prop={name}  fuc={propfromChild}/>
+        <h2>{age}</h2>
         </>
     )
 };
